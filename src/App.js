cbcc53d8e0f39b5cc73a338fb5501e63fe06d4ea
Timothy Condello <timcon@amazon.com>
2021-10-20 11:18:34 -0400

    update with gray background

--- src/App.js
+++ src/App.js
@@ -10,41 +10,43 @@ import {
 
 function App() {
   return (
-      <Router>
-          <div className="relative min-h-screen md:flex">
-              {/*sidebar*/}
-              <div
-                  className={"bg-blue-800 text-blue-100 w-64 space-y-6 py-7 px-2 absolute inset-y-0 left-0 transform md:relative md:translate-x-0 transition duration-200 ease-in-out"}>
-                  {/*logo*/}
-                  <a className="text-white flex items-center space-x-2 px-2" href="/">
-                      <svg className="w-8 h-8" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20"
-                           fill="currentColor">
-                          <path fill-rule="evenodd"
-                                d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z"
-                                clip-rule="evenodd"/>
-                      </svg>
-                      <span className="text-2xl font-extrabold">IDP</span>
-                  </a>
-                  <nav>
-                      <Link to="/"
-                         className="block py-2.5 px-4 hover:bg-blue-700 hover:text-white rounded transition duration-200">Home</Link>
-                      <Link to="/banking"
-                         className="block py-2.5 px-4 hover:bg-blue-700 hover:text-white rounded transition duration-200">Banking</Link>
-                      <Link to="/healthcare"
-                         className="block py-2.5 px-4 hover:bg-blue-700 hover:text-white rounded transition duration-200">Healthcare</Link>
-                      <Link to="/insurance"
-                         className="block py-2.5 px-4 hover:bg-blue-700 hover:text-white rounded transition duration-200">Insurance</Link>
-                  </nav>
+      <div className="bg-gray-100">
+          <Router>
+              <div className="relative min-h-screen md:flex">
+                  {/*sidebar*/}
+                  <div
+                      className={"bg-blue-800 text-blue-100 w-64 space-y-6 py-7 px-2 absolute inset-y-0 left-0 transform md:relative md:translate-x-0 transition duration-200 ease-in-out"}>
+                      {/*logo*/}
+                      <a className="text-white flex items-center space-x-2 px-2" href="/">
+                          <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                               fill="currentColor">
+                              <path fill-rule="evenodd"
+                                    d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z"
+                                    clip-rule="evenodd"/>
+                          </svg>
+                          <span className="text-2xl font-extrabold">IDP</span>
+                      </a>
+                      <nav>
+                          <Link to="/"
+                             className="block py-2.5 px-4 hover:bg-blue-700 hover:text-white rounded transition duration-200">Home</Link>
+                          <Link to="/banking"
+                             className="block py-2.5 px-4 hover:bg-blue-700 hover:text-white rounded transition duration-200">Banking</Link>
+                          <Link to="/healthcare"
+                             className="block py-2.5 px-4 hover:bg-blue-700 hover:text-white rounded transition duration-200">Healthcare</Link>
+                          <Link to="/insurance"
+                             className="block py-2.5 px-4 hover:bg-blue-700 hover:text-white rounded transition duration-200">Insurance</Link>
+                      </nav>
+                  </div>
+                  <div className="p-10 text-2xl font-bold flex-1">
+                      <Switch>
+                          {routes.map((route) => (
+                              <Route key={route.path} path={route.path} component={route.component} />
+                          ))}
+                      </Switch>
+                  </div>
               </div>
-              <div className="p-10 text-2xl font-bold flex-1">
-                  <Switch>
-                      {routes.map((route) => (
-                          <Route key={route.path} path={route.path} component={route.component} />
-                      ))}
-                  </Switch>
-              </div>
-          </div>
-      </Router>
+          </Router>
+      </div>
   );
 }
 
--- src/components/views/healthcare.js
+++ src/components/views/healthcare.js
@@ -1,13 +1,47 @@
 import React from 'react';
 
+const useCase_list = [
+    {
+        name: 'Claims Processing',
+        description: 'Digitize, auto-review, auto-categorize documents',
+        path: '/'
+    },
+    {
+        name: 'Invoice Processing',
+        description: 'Auto-ingest the data and process invoice and receipts',
+        path: '/'
+    }
+]
+
 function Healthcare() {
     return (
         <>
             <div className="bg-blue-200 py-6 flex items-center justify-center min-w-screen px-4">
                 <p className="text-5xl text-gray-800">Healthcare</p>
             </div>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-x-2 gap-y-4 h-24">
+                {useCase_list.map((useCase,i) =>
+                    <div key={i} className="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20">
+                        <div>
+                            <h2 className="text-gray-800 text-3xl font-semibold">
+                                {useCase.name}
+                            </h2>
+                            <p className="mt-2 text-gray-600">
+                                {useCase.description}
+                            </p>
+                        </div>
+                        <div className="flex justify-end mt-4">
+                            <a href={useCase.path} className="text-xl font-medium text-indigo-500">
+                                See it in Action
+                            </a>
+                        </div>
+                    </div>
+
+                )}
+            </div>
         </>
     );
 }
+
 
 export default Healthcare;
