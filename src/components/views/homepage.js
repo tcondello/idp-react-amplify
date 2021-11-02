43adc88fd11ccbbf2dee155516c2f492a1167f52
Timothy Condello <timcon@amazon.com>
2021-11-02 10:36:26 -0400

    fixed links in build

--- src/components/views/homepage.js
+++ src/components/views/homepage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 //THIS IS DIFFERENT FROM MY VIEWS DONT FORGET
 function Homepage(props){
@@ -8,17 +9,16 @@ function Homepage(props){
                 <p className="text-5xl text-gray-800">Pick Your Industry</p>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-x-2 gap-y-4 h-24">
-                {props.routes.map(filteredRoute =>
-                    <div key={filteredRoute.path} className="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20">
+                {props.routes.map(routes =>
+                    <div key={routes.path} className="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20">
                         <div>
                             <h2 className="text-gray-800 text-3xl font-semibold">
-                                {filteredRoute.name}
+                                {routes.name}
                             </h2>
                         </div>
                         <div className="flex justify-end mt-4">
-                            <a href={filteredRoute.path} className="text-xl font-medium text-indigo-500">
-                                Explore Now
-                            </a>
+                            <Link to={routes.path}
+                                  className="text-xl font-medium text-indigo-500">Explore Now</Link>
                         </div>
                     </div>
 
