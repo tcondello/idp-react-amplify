f1fa8d8d5aa2bb79a30fb67b78638a3b2ddee9a8
Tim C <tcondello@users.noreply.github.com>
2022-07-07 20:37:05 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ import {
 function App() {
   return (
       <div className="bg-gray-100">
+          <>
+              <a href="https://aetion-test.auth.us-east-1.amazoncognito.com/login?client_id=5elepong4fs772k8fpi0jr7rku&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://master.d1kml5v35miqkp.amplifyapp.com/" className="href">LogIn</a>
+          </>
           <Router>
               <div className="relative min-h-screen md:flex">
                   <Sidenav routes={industries_array}/>
